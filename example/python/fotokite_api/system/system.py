4cf51b25d82541a609cac62a88a23cb5e2090fcb
Mario Ruci <marioruci15@gmail.com>
2025-09-10 13:39:57 +0200

    Auth workflow for system

--- example/python/fotokite_api/system/system.py
+++ example/python/fotokite_api/system/system.py
@@ -6,14 +6,14 @@ from typing import Callable, cast
 import requests
 from websockets.sync.client import connect
 
-from fotokite_api.utils import BASE_REST_API_URL, BASE_WEBSOCKET_API_URL
+from fotokite_api.utils import BASE_REST_API_URL, BASE_WEBSOCKET_API_URL, retrieve_auth_token, API_KEY
 
 SystemMessage = dict[str, object]
 
 
-def system_info() -> SystemMessage:
+def system_info(access_token: str = "") -> SystemMessage:
     try:
-        response = requests.get(f"{BASE_REST_API_URL}/info/system")
+        response = requests.get(f"{BASE_REST_API_URL}/info/system", headers={"Authorization": f"Bearer {access_token}"})
         if response.status_code == 200:
             return cast(SystemMessage, response.json())
         else:
@@ -27,10 +27,11 @@ def system_info() -> SystemMessage:
 def system_telemetry(
     on_message_callback: Callable[[SystemMessage], None],
     max_messages: int | None = None,
+    access_token: str = ""
 ) -> None:
-    ws_url = f"{BASE_WEBSOCKET_API_URL}/telemetry/system/subscribe"
+    ws_url = f"{BASE_WEBSOCKET_API_URL}/telemetry/system/subscribe?access_token={access_token}"
     try:
-        with connect(ws_url) as websocket:
+        with connect(ws_url, additional_headers={"Authorization": f"Bearer {access_token}"}) as websocket:
             logging.info("Connected to system telemetry")
             count = 0
             while True:
@@ -65,8 +66,13 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
+    auth_token = retrieve_auth_token(API_KEY)
+    if auth_token is None:
+        logging.error("Failed to retrieve authentication token. Exiting.")
+        exit(1)
+
     if args.action == "info":
-        info = system_info()
+        info = system_info(auth_token)
         logging.info("System Info:\n%s", json.dumps(info, indent=2))
     elif args.action == "telemetry":
-        system_telemetry(_handle_telemetry)
+        system_telemetry(_handle_telemetry, None, auth_token)
--- example/python/fotokite_api/utils.py
+++ example/python/fotokite_api/utils.py
@@ -1,2 +1,22 @@
-BASE_REST_API_URL = "http://192.168.2.100:3128/api/v0"
-BASE_WEBSOCKET_API_URL = "ws://192.168.2.100:3128/api/v0"
+import requests
+import logging
+
+BASE_REST_API_URL = "http://localhost:3128/api/v0"
+BASE_WEBSOCKET_API_URL = "ws://localhost:3128/api/v0"
+API_KEY = "*****"
+
+def retrieve_auth_token(api_key: str) -> str | None:
+    try:
+        response = requests.post(
+            f"{BASE_REST_API_URL}/authentication/tokens/request",
+            json={"client_secret": api_key},
+        )
+        if response.status_code == 200:
+            return str(response.json().get("access_token") or "")
+        else:
+            response.raise_for_status()
+    except Exception as e:
+        logging.error(f"Error retrieving an auth token: {e}")
+        return None
+
+    return None
