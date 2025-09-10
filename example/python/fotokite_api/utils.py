77e6254e6ab8368e29ca5d7eb66037f47f986dfd
Mario Ruci <marioruci15@gmail.com>
2025-09-10 14:05:41 +0200

    Auth workflow for demos

--- example/python/fotokite_api/utils.py
+++ example/python/fotokite_api/utils.py
@@ -2,8 +2,8 @@ import logging
 
 import requests
 
-BASE_REST_API_URL = "http://localhost:3128/api/v0"
-BASE_WEBSOCKET_API_URL = "ws://localhost:3128/api/v0"
+BASE_REST_API_URL = "http://192.168.2.100:3128/api/v0"
+BASE_WEBSOCKET_API_URL = "ws://192.168.2.100:3128/api/v0"
 API_KEY = "*******"
 
 
